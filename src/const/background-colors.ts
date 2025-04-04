67a2350bbb49ed90d934765423804a119c95a6df
DaniPoot <danielpoot.uc2@gmail.com>
2025-04-04 09:54:24 -0600

    add background-colors.ts and button-types.ts constants for task and idle time styling in the application

---
+++ src/const/background-colors.ts
@@ -0,0 +1,7 @@
+export const BACKGROUND_COLORS = {
+  StartTask: '#FD500B',
+  EndTask: '#ED2929',
+  StartNextTask: '#FD500B',
+  StartIdleTime: '#FD8D60',
+  EndIdleTime: '#ED2929',
+};
---
+++ src/const/button-types.ts
@@ -0,0 +1,7 @@
+export const BUTTON_TYPES = {
+  StartTask: 'StartTask',
+  EndTask: 'EndTask',
+  StartNextTask: 'StartNextTask',
+  StartIdleTime: 'StartIdleTime',
+  EndIdleTime: 'EndIdleTime',
+};
