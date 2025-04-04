fcbf73b5c420b99b8242e4bb08fc882fb0151cab
DaniPoot <danielpoot.uc2@gmail.com>
2025-04-04 09:31:58 -0600

    Update config files to use typescript instead of javascript

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,7 +3,7 @@ export const EPLOYEES_BOARD_ID = 2772063654;
 export const PEOPLE_COLUMN_ID = 'person';
 export const MONDAY_AUTH_TOKEN =
   'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIyNTk5MjQ5NCwiYWFpIjoxMSwidWlkIjozMTc0MjU2NywiaWFkIjoiMjAyMy0wMS0xOVQxNzo0NTowOS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTE4MjQwMzIsInJnbiI6InVzZTEifQ.f7cLiwfdIsuL15yVD0QFnm06JUCIIutMojHSyEJ63Ls';
-export const MONDAY_API_VERSION = '2023-10';
+export const MONDAY_API_VERSION = '2025-04';
 
 export const DEFAULT_TIMEZONE = 'America/Chicago';
 export const DEFAULT_TIME_FORMAT = 'MMM Do h:mm:ss a';
@@ -13,4 +13,4 @@ export const IDLE_NAME = 'Idle';
 export const API_TIMEOUT = 60000; // in ms
 export const DEFAULT_ITEMS_LIMIT = 100;
 
-export const LOGS_API= 'https://e7bad-service-26041556-c4458420.us.monday.app/logs'
+export const LOGS_API= 'https://live1-service-20441992-ee320d92.us.monday.app/logs';
